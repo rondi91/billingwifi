70fcb7fbe78542ea7da3144d60d3ff2b5475d734
rondi91 <rondie91@gmail.com>
2023-02-10 13:47:11 +0700

    MEMBUAT TEMPLATE BASIC ELA ADMIN

---
+++ resources/views/back/layouts/leftpanel.blade.php
@@ -0,0 +1,19 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                </li>
+                <li class="menu-title">UI elements</li><!-- /.menu-title -->
+                
+                <li>
+                    <a href="widgets.html"> <i class="menu-icon ti-email"></i>User </a>
+                </li>
+               
+           
+                
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>
